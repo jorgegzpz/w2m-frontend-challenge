db1b55342310a5707c1b57db8b2ccf58b612ed9d
jorge <jorgegp@proton.me>
2023-11-02 22:46:53 +0100

    refactor: remove the img field

--- src/app/components/heroes/model/hero.model.ts
+++ src/app/components/heroes/model/hero.model.ts
@@ -2,12 +2,10 @@ export interface Hero {
   id: number;
   name: string;
   powers?: string[];
-  img?: string;
 }
 
 export enum HeroColum {
   id = 'Id',
-  img = 'Image',
   name = 'Name',
   powers = 'Powers',
 }
