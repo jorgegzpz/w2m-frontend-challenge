c287917620db3c0f4bb8a4085f6363eb24d767c1
jorge <jorgegp@proton.me>
2023-11-02 22:46:30 +0100

    fix: change the list of heroes

--- src/app/components/heroes/services/heroes-handler.service.ts
+++ src/app/components/heroes/services/heroes-handler.service.ts
@@ -1,33 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Hero } from '../model/hero.model';
+import { HEROES_MOCK_LIST } from '../model/heroes-mock-list';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroesHandlerService {
-  private heroesList: Hero[] = [
-    { id: 0, name: 'Ant-Man' },
-    { id: 1, name: 'Aquaman' },
-    { id: 2, name: 'Batman' },
-    { id: 3, name: 'Black Panther' },
-    { id: 4, name: 'Captain America' },
-    { id: 5, name: 'Catwoman' },
-    { id: 6, name: 'Daredevil' },
-    { id: 7, name: 'Doctor Strange' },
-    { id: 8, name: 'Green Lantern' },
-    { id: 9, name: 'Hawkeye' },
-    { id: 10, name: 'Hellboy' },
-    { id: 11, name: 'Hulk' },
-    { id: 12, name: 'Iron Fist' },
-    { id: 13, name: 'Iron Man' },
-    { id: 14, name: 'Spider-Man' },
-    { id: 15, name: 'Miles Morales' },
-    { id: 16, name: 'Superman' },
-    { id: 17, name: 'Thor' },
-    { id: 18, name: 'Watchmen' },
-    { id: 19, name: 'Wolverine' },
-    { id: 20, name: 'Wonder Woman' },
-  ];
+  private heroesList: Hero[] = HEROES_MOCK_LIST;
 
   getHeroesList(): Hero[] {
     return this.heroesList;
@@ -42,6 +21,7 @@ export class HeroesHandlerService {
   }
 
   addHero(hero: Hero) {
+    debugger;
     this.heroesList.push({
       ...hero,
       id: this.heroesList.length,
@@ -57,6 +37,7 @@ export class HeroesHandlerService {
   }
 
   removeHero(id: number): Hero[] {
+    debugger;
     const heroToRemoveIndex = this.heroesList.findIndex(hero => hero.id === id);
     return this.heroesList.splice(heroToRemoveIndex);
   }
