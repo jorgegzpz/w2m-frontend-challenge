e844003badb187a49c37801c33729cdb55552c65
Jorge Gonzalez Perez <jorgegp@proton.me>
2024-11-23 00:35:28 +0100

    fix eslint config errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,55 +1,60 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [{
-    ignores: [
-        "projects/**/*",
-        "**/package.json",
-        "**/package-lock.json",
-        "**/dist",
-        "**/node_modules",
-    ],
-}, ...compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@angular-eslint/recommended",
-    "plugin:@angular-eslint/template/process-inline-templates",
-).map(config => ({
-    ...config,
-    files: ["**/*.ts"],
-})), {
-    files: ["**/*.ts"],
+export default [
+  {
+    ignores: ['projects/**/*', '**/package.json', '**/package-lock.json', '**/dist', '**/node_modules'],
+  },
+  ...compat
+    .extends(
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@angular-eslint/recommended',
+      'plugin:@angular-eslint/template/process-inline-templates'
+    )
+    .map(config => ({
+      ...config,
+      files: ['**/*.ts'],
+    })),
+  {
+    files: ['**/*.ts'],
 
     rules: {
-        "@angular-eslint/directive-selector": ["error", {
-            type: "attribute",
-            prefix: "app",
-            style: "camelCase",
-        }],
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
 
-        "@angular-eslint/component-selector": ["error", {
-            type: "element",
-            prefix: "app",
-            style: "kebab-case",
-        }],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
     },
-}, ...compat.extends(
-    "plugin:@angular-eslint/template/recommended",
-    "plugin:@angular-eslint/template/accessibility",
-).map(config => ({
+  },
+  ...compat.extends('plugin:@angular-eslint/template/recommended', 'plugin:@angular-eslint/template/accessibility').map(config => ({
     ...config,
-    files: ["**/*.html"],
-})), {
-    files: ["**/*.html"],
+    files: ['**/*.html'],
+  })),
+  {
+    files: ['**/*.html'],
     rules: {},
-}];
+  },
+];
